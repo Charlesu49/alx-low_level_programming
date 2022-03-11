a256de87f0d71cd4b22daa26ef27803c6fa2ee86
Charlesu49 <charlesu49@gmail.com>
2022-03-11 09:58:22 -0800

    problem 8 correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - prints exactly \"and that piece of art is useful
- * \" - Dora Korpar, 2015-10-19
- * followed by a new line to the standard error
+ * main - prints exactly \"and that piece of art is useful\" - Dora
+ * Korpar, 2015-10-19 followed by a new line to the standard error
  * Return- 1 if successful
  */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	return (1);
 }
